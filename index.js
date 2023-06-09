9f8f0fd5dbe9cfc8290db8b8469e73cabe43e3d3
Alejandra Paez <alepaezdev@gmail.com>
2023-06-08 22:07:28 -0600

    feat: middlewares

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+const { response } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 
 // App de express
 const app = express();
+
+// Parseo a json -> Middleware
+
+
+
+// Primer middleware
 app.use(express.json());
+
+// Segundo middleare
+app.use((request, response, next) => {
+  console.log("Primer middleware");  
+  next() // Continuar
+})
+app.use((request, response, next) => {
+  console.log("segundo middleware");
+  next();
+})
+
+const middlewareEncapsulado = (req, res, next) => {
+  console.log("Middleware encapsulado para un endpoint en especifico");
+  // next();
+}
+/**
+ * Middlewares
+ * - Nos sirve para poder realizar codigo antes de los endpoints.
+ * - Tienen acceso a la request, a la response.
+ * - Tienen una palabra llamada next() -> indica que puedes continuar
+ */
 
 // Url de base de datos
 const databaseURL = "mongodb+srv://ale:a123456@kodemia.fh2syr1.mongodb.net/kodemia"
@@ -42,6 +70,9 @@ const koderSchema = new mongoose.Schema({
   sex: {
     type: String,
     enum: ["f", "m", "o"]
+  },
+  createdAt: {
+    type: Date
   }
 })
 
@@ -78,8 +109,11 @@ app.get("/koders", async (req, res) => {
 })
 
 // Crear un koder
-app.post("/koders", async (req, res) => {
-  console.log("body -->", req.body)
+// parametros -> ruta, callback
+// si queremos un middleware para ese endpoint en especifico, se pone como 2do parametro
+// ruta, middleware, callback
+app.post("/koders", middlewareEncapsulado, async (req, res) => {
+  console.log("body en el endpoint de POST-->", req.body)
   /**
    * - Request -
    * Params
@@ -109,6 +143,38 @@ app.post("/koders", async (req, res) => {
  * ruta: 
  * Model.findById()
  */
+
+// Endpoint donde eliminemos al koder
+// ruta -> /koders/:id
+// metodo -> delete;
+
+app.delete("/koders/:id", async (req, res) => {
+  const { id } = req.params
+  try {
+    const deletedKoder = await Koder.findByIdAndDelete(id)
+    let status = 200;
+    const responseParams = {
+      success: true,
+      message: "El koder fue eliminado exitosamente"
+    }
+    // !variable -> variable === null, variable === undefined, variable === false
+    if(!deletedKoder) {
+      responseParams.success = false;
+      responseParams.message = "El koder no fue encontrado";
+      status = 404
+    }
+    // Mi codigo continua
+    res.status(status);
+    res.json(responseParams)
+    
+  }catch(err) {
+    res.status(400);
+    res.json({
+      success: false,
+      message: err.message
+    })
+  }
+})
 // Conectar a la base de datos
 mongoose.connect(databaseURL)
 .then(() => {
